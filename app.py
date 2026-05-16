6f308b9c64ed28c38d211fdfad37854cede11b7e
Haripriya <marupakaharipriya@gmail.com>
2026-05-16 21:15:14 +0530

    Updated app connection

--- app.py
+++ app.py
@@ -1,7 +1,4 @@
-from flask import Flask
+from backend.app import app
 
-app = Flask(__name__)
-
-@app.route("/")
-def home():
-    return "Sales Forecast AI is running successfully!"
+if __name__ == "__main__":
+    app.run()
